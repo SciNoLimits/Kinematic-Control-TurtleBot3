a58724c72ddc7e48d7e636969d17297a0643506b
SciNoLimits <prazwaldutta7@gmail.com>
2026-07-29 17:36:59 +0000

    Initial ROS 2 package

---
+++ kinematic_control_turtle_bot_3/minimal_velocity_publisher.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class MinimalVelocityPublisher(Node):
+    def __init__(self):
+        super().__init__("minimal_velocity_publisher")
+        self.publisher_ = self.create_publisher(msg_type=Twist, topic='/cmd_vel', qos_profile=10)
+        self.timer_ = self.create_timer(timer_period_sec=0.5, callback=self.send_velocity_cmd)
+        self.get_logger().info("Minimal Velocity Publisher has been started")
+
+
+    def send_velocity_cmd(self):
+        msg = Twist()
+        msg.linear.x = 0.1      # drive forward at 0.1 m/s
+        msg.angular.z = 0.0
+        self.publisher_.publish(msg=msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MinimalVelocityPublisher()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__=="__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'kinematic_control_turtle_bot_3'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='scinolimits',
+    maintainer_email='scinolimits@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'my_node = kinematic_control_turtle_bot_3.my_node:main',
+            'minimal_velocity_publisher = kinematic_control_turtle_bot_3.minimal_velocity_publisher:main',
+        ],
+    },
+)
